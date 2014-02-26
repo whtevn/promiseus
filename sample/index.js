34db7f4a78b75fe93efc18a76f03a463297ad466
evan <evan@sdicg.com>
2014-02-26 11:45:29 -0500

    added required packages for lib members, added sample project for testing purposes, updated apiResource to be included in the new environment schema

--- sample/index.js
+++ sample/index.js
@@ -1,18 +1,28 @@
 var env = require('../lib/envGen'),
-		queue = require('../lib/queue');
-		ses = require('../lib/ses');
-		bucket = require('../lib/bucket');
-		apiResource = require('../lib/apiResource');
+		queue = require('../lib/queue'),
+		ses = require('../lib/ses'),
+		bucket = require('../lib/bucket'),
+		apiResource = require('../lib/apiResource'),
+		pinwheel = new apiResource(),
+		clever   = new apiResource();
 
-env = new env('./sample.js');
+env = new env('./config.json');
 
 env.register('queue', queue).then(function(q){
 	q.register('user', 'teacher');
-	console.log(q.user);
 	return q;
 })
 
+env.register('queue', queue);
 env.register('ses', ses);
-env.register('celltrust', celltrust);
-env.register('mongo', mongoclient);
-env.register('apiResource', apiResource);
+env.register('bucket', bucket);
+env.register('pinwheel', pinwheel);
+env.register('clever', clever);
+
+env.clever.then(function(clever){
+	clever.get('students').then(function(response){
+		console.log(response);
+	});
+});
+
+module.exports = env;
