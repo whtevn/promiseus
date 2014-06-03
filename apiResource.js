5f6edbd95f7556bff81338f09688d2ce0e00332e
evan <evan@sdicg.com>
2014-06-03 10:27:28 -0400

    making https agent active only if ssl request

--- apiResource.js
+++ apiResource.js
@@ -42,7 +42,9 @@ function resource(options){
 		extend(this.requestBase, options.options);
 	}
 
-	this.requestBase.agent = new httpAgent.Agent(this.requestBase);
+	if(options.ssl){
+		this.requestBase.agent = new httpAgent.Agent(this.requestBase);
+	}
 
 	if(options.headers.request||options.headers.response){
 		this.requestBase.headers.request  = (options.headers.request ||{})	
